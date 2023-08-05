20c2250f5065a358542c89c32ae5994d5f281cbe
Ayush <ayush.10219011622@ipu.ac.in>
2023-08-06 00:48:07 +0530

    06-08-23

--- 6 month Java/area.java
+++ 6 month Java/area.java
@@ -13,22 +13,35 @@ public class area
     //     System.out.print("Area of triangle = "+ area);
     // }
 
+    // public static void main(String[] args) 
+    // {
+        
+    //     Scanner sc=new Scanner(System.in);
+    //     float a,b,c;
+    //     System.out.print("Enter side1 = ");
+    //     a = sc.nextFloat();
+    //     System.out.print ("Enter side2 = ");
+    //     b=sc.nextFloat();
+    //     System.out.print ("Enter side3 = ");
+    //     c=sc.nextFloat();
+    //     double peri=(a+b+c)/2;
+    //     double area=Math.sqrt(peri*(peri-a)*(peri-b)*(peri-c));
+    //     System.out.println("Perimeter of scaler triangle = "+peri);
+    //     System.out.println("Area of scaler triangle = "+area);
+    // }
+
     public static void main(String[] args) 
     {
-        
         Scanner sc=new Scanner(System.in);
-        float a,b,c;
-        System.out.print("Enter side1 = ");
-        a = sc.nextFloat();
-        System.out.print ("Enter side2 = ");
+        double a,b,c,r1,r2;
+        System.out.print("Enter value of a, b & c = ");
+        a=sc.nextFloat();
         b=sc.nextFloat();
-        System.out.print ("Enter side3 = ");
         c=sc.nextFloat();
-        double peri=(a+b+c)/2;
-        double area=Math.sqrt(peri*(peri-a)*(peri-b)*(peri-c));
-        System.out.println("Perimeter of scaler triangle = "+peri);
-        System.out.println("Area of scaler triangle = "+area);
-
+        r1=((-b)+ (Math.sqrt((b*b)-4*a*c)))/(2*a);
+        r2=((-b)- (Math.sqrt((b*b)-4*a*c)))/(2*a);
+        System.out.println("Root of given function = "+r1);
+        System.out.println("Root of given function = "+r2);
 
     }
 }
---
+++ 6 month Java/print.java
@@ -0,0 +1,14 @@
+public class print
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        a=6;
+        b=20;
+        c=a*b;
+        System.out.println("Multiply of "+a +" & "+b +" = "+c);
+        System.out.println(b+a+" Multiply ");
+        System.out.println("Multiply "+a+b);
+        System.out.println("Multiply "+(a+b));
+    } 
+}
