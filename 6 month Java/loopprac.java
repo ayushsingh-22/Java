1307f783022611b75d3a57eccb22e5515d341adb
Ayush <ayush.10219011622@ipu.ac.in>
2023-08-09 01:46:05 +0530

    09-08-23

--- 6 month Java/loopprac.java
+++ 6 month Java/loopprac.java
@@ -7,125 +7,142 @@ public class loopprac
         
         // Multiplication of number;
         
-        // int num;
-        // System.out.print("Enter number: ");
-        // num=sc.nextInt();
+       /*  int num;
+        System.out.print("Enter number: ");
+        num=sc.nextInt();
 
-        // for(int i=1;i<11;i++)
-        // {
-        //     int tab=num*i;
-        //     System.out.printf("%d x %d = %d\n",num,i,tab);
-        // }
+        for(int i=1;i<11;i++)
+        {
+            int tab=num*i;
+            System.out.printf("%d x %d = %d\n",num,i,tab);
+        }
+
+        */
 
         // Sum of n-number;
 
-        // int n;
-        // int sum = 0;
-        // int i=1;
+       /*  int n;
+        int sum = 0;
+        int i=1;
 
-        // System.out.print("Enter number = ");
-        // n=sc.nextInt();
+        System.out.print("Enter number = ");
+        n=sc.nextInt();
 
-        // while(i<=n)
-        // {
-        //     sum=sum+i;
-        //     i++;
-        // }
-        // System.out.printf("Sum of %d = %d",n,sum);
+        while(i<=n)
+        {
+            sum=sum+i;
+            i++;
+        }
+        System.out.printf("Sum of %d = %d",n,sum); */
 
         // factorial of given number;
 
-    //     int fact=1,n;
+    /*     int fact=1,n;
 
-    //     System.out.print("Factorial of: ");
-    //     n=sc.nextInt();
+        System.out.print("Factorial of: ");
+        n=sc.nextInt();
 
-    //    for(int i=1; i<=n;i++)
-    //    {
-    //       fact = fact*i;
-    //    }
-    //     System.out.printf("Fcatorial of %d = %d",n,fact);
+       for(int i=1; i<=n;i++)
+       {
+          fact = fact*i;
+       }
+        System.out.printf("Fcatorial of %d = %d",n,fact);
+
+    */
 
         // print each digit of number.
         
-        // System.out.print("Enter digit = ");
-        // int digit=sc.nextInt();
+    /*     System.out.print("Enter digit = ");
+        int digit=sc.nextInt();
 
-        // while(digit>0)
-        // {
-        //     int rem=digit%10;
-        //     System.out.print(rem+",");
-        //     digit=digit/10;
-        // }
+        while(digit>0)
+        {
+            int rem=digit%10;
+            System.out.print(rem+",");
+            digit=digit/10;
+        }
+    */
 
         // count digit of number;
 
-        // System.out.print("Enter number = ");
-        // int n=sc.nextInt();
-        // int temp = n; 
-        // int i=0;
+    /*     
+        System.out.print("Enter number = ");
+        int n=sc.nextInt();
+        int temp = n; 
+        int i=0;
 
-        // while(n>0)
-        // {
-        //     n=n/10;
-        //     i++;
-        // }
-        // System.out.printf("Total number of digit in %d is %d",temp,i);
+        while(n>0)
+        {
+            n=n/10;
+            i++;
+        }
+        System.out.printf("Total number of digit in %d is %d",temp,i);
+
+    */
 
         // find 3 digit armstrong number;
 
-        // System.out.print("Enter digit = ");
-        // int digit=sc.nextInt();
-        // int tempa = digit;
-        // int sum=0;
+    /* 
+        System.out.print("Enter digit = ");
+        int digit=sc.nextInt();
+        int tempa = digit;
+        int sum=0;
 
-        // while(digit>0)
-        // {
-        //     int rem=digit%10;
-        //     digit=digit/10;
-        //     sum=(rem*rem*rem)+sum;
-        // }
+        while(digit>0)
+        {
+            int rem=digit%10;
+            digit=digit/10;
+            sum=(rem*rem*rem)+sum;
+        }
 
-        // if (sum == tempa)
-        // {System.out.printf("%d is amstrong number.",tempa);}
-        // else{System.out.printf("%d is not amstrong number.",tempa);}
+        if (sum == tempa)
+        {System.out.printf("%d is amstrong number.",tempa);}
+        else{System.out.printf("%d is not amstrong number.",tempa);}
+
+    */
 
         // print reverse of given digit.
 
-        // System.out.print("Enter number = ");
-        // int orig=sc.nextInt();
-        // int tamp=orig;
+    /*
+        System.out.print("Enter number = ");
+        int orig=sc.nextInt();
+        int tamp=orig;
 
-        // System.out.printf("Reverse digit of %d is ",orig);
-        // while(orig>0)
-        // {
-        //     int re=orig%10;
-        //     System.out.print(re);
-        //     orig=orig/10;
-        // }
+        System.out.printf("Reverse digit of %d is ",orig);
+        while(orig>0)
+        {
+            int re=orig%10;
+            System.out.print(re);
+            orig=orig/10;
+        }
+    */
 
         // palindrome or not
 
-        // System.out.print("Enter number = ");
-        // int pal=sc.nextInt();
-        // int ran=pal;
-        // int temp=0;
+    /*     
+        System.out.print("Enter number = ");
+        int pal=sc.nextInt();
+        int ran=pal;
+        int temp=0;
        
-        // while(pal>0)
-        // {
-        //     int rem=pal%10;
-        //     temp=temp*10+rem;
-        //     pal=pal/10;
-        // }
-        // System.out.println("Reversed number = "+temp);
+        while(pal>0)
+        {
+            int rem=pal%10;
+            temp=temp*10+rem;
+            pal=pal/10;
+        }
+        System.out.println("Reversed number = "+temp);
 
-        // if(ran==temp)
-        // {System.out.println("Palindrome Number.");}
-        // else
-        // {System.out.println("Not Palindrome Number.");}
+        if(ran==temp)
+        {System.out.println("Palindrome Number.");}
+        else
+        {System.out.println("Not Palindrome Number.");}
+
+    */
 
         // display no. in words with tailling zero if present
 
+    /*    
         System.out.print("Enter number = ");
         String str=sc.next();
         String rev="";
@@ -137,6 +154,57 @@ public class loopprac
 
         System.out.println("Reversed digit = "+rev);
 
+    */
+
+        // print A.P. Series;
+
+    /* 
+        System.out.print("Enter first digit = ");
+        int a=sc.nextInt();
+        System.out.print("Enter number of term = ");
+        int n=sc.nextInt();
+        System.out.print("Enter difference between 2 term = ");
+        int d=sc.nextInt();
+
+        for (int i=1;i<=n;i++)
+        {
+            System.out.print(a+",");
+            a=a+d;
+        }
+    */
+
+        // Print G.P. Series:
+    /* 
+        System.out.print("Enter first digit = ");
+        int a=sc.nextInt();
+        System.out.print("Enter number of term = ");
+        int n=sc.nextInt();
+        System.out.print("Enter difference between 2 term = ");
+        int d=sc.nextInt();
+
+        for (int i=1;i<=n;i++)
+        {
+            System.out.print(a+",");
+            a=a*d;
+        }
+    */
+
+        // Fibonacci series;
+
+        System.out.print("Enter number of term = ");
+        int n=sc.nextInt(); 
+        int a=0;
+        int b=1;
+        System.out.print(a+","+b+",");
+
+        for (int i=1;i<=n-2;i++)
+        {
+            
+            int c=a+b;
+            System.out.print(c+",");
+            a=b;
+            b=c;
+        }
 
     }
 }
---
+++ 6 month Java/nestedloopprac.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class nestedloopprac 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+    /*     
+        for (int i=1;i<6;i++)
+        {
+            for(int j=1;j<6;j++)
+            {
+
+                System.out.print(j+" ");
+            }
+            System.out.println();            
+        }
+    */
+
+    /* 
+        for (int i=1;i<6;i++)
+        {
+            for(int j=1;j<6;j++)
+            {
+
+                System.out.print((j+i)+" ");
+            }
+            System.out.println();   
+        }
+    */
+
+    /*     
+        int count =0;
+        for (int i=1;i<6;i++)
+        {
+            for(int j=1;j<6;j++)
+            {
+                count++;
+                System.out.format("%2d ",count);
+            }
+            System.out.println(); 
+        }
+    */
+
+    }
+}
