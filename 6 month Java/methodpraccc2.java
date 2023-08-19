7fb1c54a3af09208713ce18c28345b57bce615f6
Ayush <ayush.10219011622@ipu.ac.in>
2023-08-19 23:28:06 +0530

    19-08-23

--- 6 month Java/methodpraccc2.java
+++ 6 month Java/methodpraccc2.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class methodpraccc2
 {
+   /*
     static boolean primechek(int n)
     {
         for (int i=2; i<n/2;i++)
@@ -24,4 +25,55 @@ public class methodpraccc2
         
         sc.close();
     }
+    */
+
+    /*
+    static int gcdcheck(int n1, int n2)
+    {
+        while (n1!=n2)
+        {
+            if (n1>n2) 
+            {
+                n1=n1-n2;
+            }
+            else n2=n2-n1;
+        }
+        return n1;
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter value of x & y = ");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+        int m=gcdcheck(x, y);
+        System.out.println("Factor of "+x+ " & "+y+" = "+m);
+    }
+    */
+
+    static int maxelement(int a[])
+    {
+        int max = a[0];
+        for(int i=1;i<a.length;i++)
+        {
+            if(a[i]>max)
+            {
+                max=a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) 
+    {
+        int b[]={2,5,4,6,12,38,22,24,29};
+
+        int c = maxelement(b);
+        System.out.println("Maximum element of array = "+c);
+        
+    }
 }
+
