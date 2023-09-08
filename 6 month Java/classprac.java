48bb7ddafbcc51f55827647c828bda4837041b79
Ayush <ayush.10219011622@ipu.ac.in>
2023-09-08 21:52:27 +0530

    08-09-2023

--- 6 month Java/classprac.java
+++ 6 month Java/classprac.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 // calculated area & perimeter of circle
 
-/*
+/* 
 class circle
 {
     public double radius;
@@ -16,6 +16,10 @@ class circle
     {
         return 2*Math.PI*radius;
     }
+    public double circumference()
+    {
+        return perimeter();
+    }
 }
 
 public class classprac 
@@ -24,8 +28,12 @@ public class classprac
     {
         Scanner sc = new Scanner(System.in);
         circle c = new circle();
-        c.radius =5;
-        System.out.println(c.area());
+        System.out.printf("Enter raius of circle = ");
+        c.radius = sc.nextInt();
+        System.out.println("Area of circle = "+c.area());
+        System.out.println("Perimeter of circle = "+c.perimeter());
+        System.out.println("Circumference of circle = "+c.circumference());
+        sc.close();
     }
     
 }
@@ -33,6 +41,7 @@ public class classprac
 
 // calculate perimeter and area of rectangle 
 
+/* 
 class rectangle
 {
     public int length;
@@ -64,5 +73,50 @@ class classprac
         System.out.println("Area of rectangle = "+rc.area());
         System.out.println("Area of rectangle = "+rc.perimeter());
         sc.close();
+    }
+}
+*/
+
+class cyclinder 
+{
+    public int height;
+    public int radius;
+
+    public float lidarea()
+    {
+        double area = Math.PI*radius*radius;
+        return (float)area;
+    }
+
+    public float volume()
+    {
+        float volume = height*radius*radius*(float)Math.PI;
+        return volume;
+    }
+
+    public float area()
+    {
+        float area = 2*(float)lidarea()+2*radius*height*(float)Math.PI;
+        return area;
+    }
+}
+
+class classprac
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        cyclinder cy=new cyclinder();
+
+        System.out.printf("Enter radius = ");
+        cy.radius = sc.nextInt();
+        System.out.printf("Enter height = ");
+        cy.height = sc.nextInt();
+        
+        System.out.println("Area of cyclinder = "+cy.area());
+        System.out.println("Volume of cyclinder = "+cy.volume());
+        System.out.println("Base area of cyclinder = "+cy.lidarea());
+        sc.close();
+
     }
 }
