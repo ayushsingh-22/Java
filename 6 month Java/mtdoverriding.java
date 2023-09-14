096d7fe348b58c728c290ff03e32f1f978316f3e
Ayush <ayush.10219011622@ipu.ac.in>
2023-09-15 00:17:01 +0530

    14-09-2023

--- 6 month Java/mtdoverriding.java
+++ 6 month Java/mtdoverriding.java
@@ -12,7 +12,8 @@ class update extends teacher
     public void addmtd(int a, int b)
     {
         int c = a+b;
-        System.out.printf("Add of %d & %d = %d",a,b,c);
+        
+          System.out.println("Add of "+a+" & "+b+ " = "+c);
         System.out.println();
     }
 }
---
+++ 6 month Java/tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+class update extends teacher
+{
+    public void addmtd(int a, int b)
+    {
+        int c = a+b;
+        
+          System.out.println("Add of "+a"& "+b "= "+c);
+        System.out.println();
+    }
+}
