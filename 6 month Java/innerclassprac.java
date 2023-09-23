3e9b4f69fd8f6fa6e8e2132d5f2eeceb7e6fa722
Ayush <ayush.10219011622@ipu.ac.in>
2023-09-23 11:16:41 +0530

    23-09-2023

--- 6 month Java/innerclassprac.java
+++ 6 month Java/innerclassprac.java
@@ -1,3 +1,6 @@
+// nested inner class
+
+/*
 class outer 
 {
     int x = 50;
@@ -25,10 +28,101 @@ class innerclassprac
 {
     public static void main(String[] args) 
     {
-   
+    //     outer os = new outer();
+    //     os.outerdis();
 
         outer.inner m = new outer().new inner();
         m.display();
         
+    }
+}
+*/
+
+
+/*
+class fun
+{
+    class sad
+    {
+        void soon()
+        {
+             System.out.print("Aayai start karta hai!!!!  --> ");
+            for(int i=0;i<4;i++)
+            {
+                System.out.print(i);
+            }
+
+            System.out.println();
+            System.out.println("Pta nahi kuch to error hai upper mai");
+            System.out.println("Bilkul sahi baat hai");
+        }
+    }
+}
+
+class innerclassprac
+{
+    public static void main(String[] args) 
+    {
+        fun.sad f=new fun().new sad();
+        f.soon();
+        
+    }
+}
+*/
+
+// local inner class 
+
+/* 
+class gun
+{
+    void price()
+    { 
+        int x=50;
+        class typee
+        {
+            void printt()
+            {
+                System.out.println("Yaha par type of gun pta chlta hai ------> "+x);
+            }
+            
+        }
+        typee pp = new typee();
+        pp.printt();
+    }
+
+}
+
+class innerclassprac
+{
+    public static void main(String[] args) 
+    {
+        gun gg = new gun();
+        gg.price();
+    }
+}
+*/
+
+// anonymous class 
+
+
+interface gun 
+{
+    void typee();
+}
+
+class innerclassprac
+{
+    public static void main(String[] args) 
+    {
+        gun gg = new gun() 
+    {
+        public void typee()
+        {
+            System.out.println("Ya par method override karna jaruri hai; Pta hana!!!");
+        }
+        
+    };
+
+        gg.typee();
     }
 }
