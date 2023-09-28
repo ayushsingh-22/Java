68eaf8e763f656a7168e2e7c90a246a4f390aff1
Ayush <ayush.10219011622@ipu.ac.in>
2023-09-28 23:43:26 +0530

    28-09-2023

--- Java-Code/array2d.java
+++ Java-Code/array2d.java
@@ -81,25 +81,26 @@ public class array2d
         // }
 
 
-        for (int i=0;i<m;i++)
-        {
-            for (int j=0;j<n;j++)
-            {
-                c[i][j]=a[i][j]*b[i][j];
-            }
-        }
+        // for (int i=0;i<m;i++)
+        // {
+        //     for (int j=0;j<n;j++)
+        //     {
+        //         c[i][j]=a[i][j]*b[i][j];
+        //     }
+        // }
 
-        System.out.println("");
+        // System.out.println("");
 
-        System.out.println("Matrix c: ");
-        for (int i=0;i<m;i++)
-        {
-            for (int j=0;j<n;j++)
-            {
-                System.out.print(c[i][j]+",");
-            }
-            System.out.println("");
-        }
+        // System.out.println("Matrix c: ");
+        // for (int i=0;i<m;i++)
+        // {
+        //     for (int j=0;j<n;j++)
+        //     {
+        //         System.out.print(c[i][j]+",");
+        //     }
+        //     System.out.println("");
+        // }
+        sc.close();
 
     }
     
--- Java-Code/tempCodeRunnerFile.java
+++ Java-Code/tempCodeRunnerFile.java
@@ -1,7 +1,19 @@
-  // for (int i=0;i<m;i++)
+ // for (int i=0;i<m;i++)
         // {
         //     for (int j=0;j<n;j++)
         //     {
-        //         c[i][j]=a[i][j]+b[i][j];
+        //         c[i][j]=a[i][j]*b[i][j];
         //     }
+        // }
+
+        // System.out.println("");
+
+        // System.out.println("Matrix c: ");
+        // for (int i=0;i<m;i++)
+        // {
+        //     for (int j=0;j<n;j++)
+        //     {
+        //         System.out.print(c[i][j]+",");
+        //     }
+        //     System.out.println("");
         // }
