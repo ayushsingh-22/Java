428b5fd848188a4fe7f8ad799b31bf5779116934
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-09-30 13:51:04 +0530

    "30-09-2023"

--- 6 month Java/hi.java
+++ 6 month Java/hi.java
@@ -2,7 +2,7 @@ public class hi
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hi!! Ayush.");
+        System.out.println("Hi!! Ayush. Kuch sahi hai kya");
         
     }
     
