53de8ee483a20e6bb39ff0aa57030c86b3ccbdc9
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-10-01 20:46:47 +0530

    01-10-2023

--- 6 month Java/methoddemo.java
+++ 6 month Java/methoddemo.java
@@ -28,6 +28,7 @@ class hi
 }
 */
 
+/* 
 class Episode 
 {
     int seriesNumber;
@@ -49,5 +50,41 @@ class methoddemo
         episode.seriesNumber = 4;
         episode.skipToNext();
         episode.skipIntro();
+    }
+}
+*/
+
+class Books
+{
+    String title;
+    String author;
+}
+class methoddemo
+{
+    public static void main(String[] args) 
+    {
+        Books[] myBooks = new Books[3];
+
+        myBooks[0] = new Books();
+        myBooks[1] = new Books();
+        myBooks[2] = new Books();
+
+        int x = 0;
+        myBooks[0].title = "The Grapes of Java";
+        myBooks[1].title = "The Java Gatsby";
+        myBooks[2].title = "The Java Cookbook";
+        myBooks[0].author = "bob";
+        myBooks[1].author = "sue";
+        myBooks[2].author = "ian";
+        
+        while (x < 3) 
+        {
+             System.out.println();
+            System.out.print(myBooks[x].title);         
+            System.out.print(" by ");         
+            System.out.println(myBooks[x].author);
+            System.out.println();
+            x = x + 1;
+        }
     }
 }
