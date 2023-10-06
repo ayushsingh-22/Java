5de92fe4857ac96fc94d61542d6af897c8caee70
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-10-06 20:22:56 +0530

    "06-10-2023"

--- 6 month Java/maxinarray.java
+++ 6 month Java/maxinarray.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class tryyyy 
+public class maxinarray 
 {
      public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-        int a[]={1,4445,23,64,41,603,12,3756930};
+        int a[]={100000000,4445,23,64,41,603,12,3730};
        
         int key=0;
 
