bb21f12bb7b737a2f7db15a741254701de9d41e4
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-10-06 23:55:11 +0530

    "06-10-2023"

--- 6 month Java/trycatchprac.java
+++ 6 month Java/trycatchprac.java
@@ -1,38 +1,29 @@
-class ListOfNumbers {
-  public int[] arr = new int[10];
-
-  public void writeList() {
-
-    try {
-      arr[10] = 11;
-    }
-    
-    catch (NumberFormatException e1) 
-    {
-        System.out.println();
-        System.out.println("NumberFormatException => " + e1.getMessage());
-    }
-    
-    catch (IndexOutOfBoundsException e2) 
-    {
-        System.out.println();
-        System.out.println("IndexOutOfBoundsException => " + e2.getMessage());
-    }
-
-    finally
-    {
-        System.out.println();
-        System.out.println("Agar ab error pta chl gya hai to sahi kar lai!!!!");
-    }
-
-  }
-}
+import java.util.Scanner;
 
 class trycatchprac 
 {
   public static void main(String[] args) 
   {
-    ListOfNumbers list = new ListOfNumbers();
-    list.writeList();
+    Scanner sc = new Scanner(System.in);
+    int a=10;
+    System.out.print("Enter value of b = ");
+    int b = sc.nextInt();
+
+    try
+    {
+      int c = a/b;
+      System.out.println("Answer aagya");
+    }
+
+    // catch(ArithmeticException e)
+    // {
+    //   System.out.println("Error ----> " +e.getMessage());
+    // }
+
+    catch(ArithmeticException e)
+    {
+      System.out.println("Error ----> " +e);
+    }
+
   }
 }
