11418cf353f5eff9eb18f2850d2107c334a77b04
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-10-12 01:04:55 +0530

    "12-10-2023"

---
+++ 6 month Java/threadsprac.java
@@ -0,0 +1,32 @@
+class threads extends Thread 
+{
+    public void run()
+    {
+        int i=0;
+        while(i<20)
+        {
+            System.out.print("Hello "+" ");
+            i++;
+        }
+        System.out.println();
+    }
+}
+
+public class threadsprac
+{
+    public static void main(String[] args) 
+    {
+        threads td = new threads();
+        td.start();
+    
+        int i=0;
+        while(i<20)
+        {
+            System.out.print("Java " + " ");
+            i++;
+        }
+        System.out.println();
+
+        
+    }
+}
