41046b41a350c1e62f28f1c60541d5aab863122e
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-10-13 15:33:55 +0530

    "13-10-2023"

--- 6 month Java/threadsprac.java
+++ 6 month Java/threadsprac.java
@@ -1,3 +1,6 @@
+// Using thraed class //
+
+/*
 class my extends Thread
 {
   public void run ()
@@ -5,7 +8,7 @@ class my extends Thread
     for (int i = 0; i < 20; i++)
     {
     	System.out.print ("Ayush");
-    	break;
+    	
     }
   }
 }
@@ -25,4 +28,62 @@ public class threadsprac
     System.out.println ();
 
   }
+}
+*/
+
+// Using run interface //
+
+/*
+class funny implements Runnable
+{
+    public void run()
+    {
+        for (int i = 0;; i++)
+            {
+    	        System.out.print ("Ayush");
+            }
+    }
+}
+
+class threadsprac
+{
+    public static void main(String[] args) 
+    {
+        funny fy = new funny();
+        Thread td = new Thread(fy);
+        td.start();
+
+        for (int i = 0;; i++)
+            {
+    	        System.out.print ("Good");
+            }
+
+    }
+}
+*/
+
+// Using run interface with different method //
+
+class threadsprac implements Runnable
+{
+    public void run()
+    {
+        for (int i = 0;i<30; i++)
+            {
+    	        System.out.println ("Ayush");
+            }
+    }
+
+    public static void main(String[] args) 
+    {
+        threadsprac fy = new threadsprac();
+        Thread td = new Thread(fy);
+        td.start();
+
+        for (int i = 0;i<25; i++)
+            {
+    	        System.out.print ("Good");
+            }
+
+    }
 }
