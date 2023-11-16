b06b8fa5fcec63afccf743d2b95780fd4fa68971
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-11-17 00:04:05 +0530

    17-11-2023

--- 6 month Java/syn.java
+++ 6 month Java/syn.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
-
 class fun
 {
     void display(String a)
     {
+    synchronized(this)
+    {
         for (int i = 0; i < a.length(); i++) 
         {
             System.out.print(a.charAt(i)+", ");
-        }
-        
+        }  
+    }
     }
 }
 
 class mythread1 extends Thread
 {
-    fun f = new fun();
+    fun f;
+
+    mythread1(fun fa)
+    {
+        f=fa;
+    } 
 
     public void run()
     {
@@ -24,19 +29,25 @@ class mythread1 extends Thread
 
 class mythread2 extends Thread
 {
-    fun f = new fun();
+    fun f;
+
+    mythread2(fun f2)
+    {
+        f=f2;
+    } 
 
     public void run()
     {
-        f.display("Rakesh");
+        f.display("fngiopt");
     }
 }
 
 public class syn {
     public static void main(String[] args) 
     { 
-        mythread1 m1 = new mythread1();
-        mythread2 m2 = new mythread2();
+        fun f = new fun();
+        mythread1 m1 = new mythread1(f);
+        mythread2 m2 = new mythread2(f);
         m1.start();
         m2.start();
     }
