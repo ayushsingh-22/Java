cdaba34b8d4cbe421e38925933e0952a744606ca
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-11-17 23:43:58 +0530

    17-11-2023

---
+++ 6 month Java/sunprac.java
@@ -0,0 +1,25 @@
+class atm
+{
+    void checkBalance(String name)
+    {
+        System.out.println(name + "checking balance" );
+    }
+
+    void withdrawl(String name, int amount)
+    {
+        System.out.println(name + "withdraw " + amount);
+    }
+}
+
+class detail extends Thread
+{
+    String name;
+    int amount;
+
+    detail(name n,)
+}
+
+public class sunprac 
+{
+    
+}
