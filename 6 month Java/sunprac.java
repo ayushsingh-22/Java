2c8f56da64342be6c9505ffbd814e75571ad0475
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-11-18 01:13:04 +0530

    18-11-2023

--- 6 month Java/sunprac.java
+++ 6 month Java/sunprac.java
@@ -1,13 +1,13 @@
 class atm
 {
-    void checkBalance(String name)
+    synchronized void checkBalance(String name)
     {
-        System.out.println(name + "checking balance" );
+        System.out.println(name + " checking balance" );
     }
 
-    void withdrawl(String name, int amount)
+    synchronized void withdrawl(String name, int amount)
     {
-        System.out.println(name + "withdraw " + amount);
+        System.out.println(name + " withdraw " + amount);
     }
 }
 
@@ -15,11 +15,35 @@ class detail extends Thread
 {
     String name;
     int amount;
+    atm aa;
+    
+    detail(String n,atm ai, int a)
+    {
+        name = n;
+        amount = a;
+        aa = ai;
+    }
 
-    detail(name n,)
+    public void useATM()
+    { 
+        aa.checkBalance(name);
+        aa.withdrawl(name, amount);
+    }
+    public void run()
+    {
+        
+       useATM();
+    }    
 }
 
 public class sunprac 
 {
-    
+    public static void main(String args[])
+    {
+        atm am = new atm();
+        detail dt1 = new detail("ayush",am, 2500);
+        dt1.start();
+        detail dt2 = new detail("Ramu",am, 500);
+        dt2.start();
+    }
 }
