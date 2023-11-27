0031f2025f9b7f5e8f8c523b3cb5437710a2d468
ayushsingh-22 <ayush.10219011622@ipu.ac.in>
2023-11-27 20:13:36 +0530

    27-11-2023

--- 6 month Java/lambda.java
+++ 6 month Java/lambda.java
@@ -1,3 +1,6 @@
+// Lambda without parameter
+
+/*
 interface Mylambda
 {
     void fun();
@@ -12,7 +15,28 @@ public class lambda
             System.out.println("My name is Raja Babu");
         };
 
-        ml.fun();
-        
+        ml.fun();  
+    }
+}
+*/
+
+// Lambda with parameter
+
+interface fun
+{
+    public int nature(int x, int y);
+}
+
+public class lambda 
+{
+    public static void main(String[] args) 
+    {
+        fun ff = (a,b)-> a+b; 
+        // {
+        //     int c=a+b;
+        //     System.out.println(c);
+        // };
+
+        System.out.println(ff.nature(45,5));  
     }
 }
